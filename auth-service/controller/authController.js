5dde525391f3b879b4bf16fc64a6b8ee4d5f4300
azdine <azdineserhani1@gmail.com>
2025-01-08 12:58:34 +0100

    add JWT authentication to login function and update dependencies

--- auth-service/controller/authController.js
+++ auth-service/controller/authController.js
@@ -3,6 +3,7 @@ import db from "../config/db.js";
 import AppError from "../utils/AppError.js";
 import bcrypt from "bcrypt";
 import { userValidationSchema } from "../validations/uservalidation.js";
+import jwt from "jsonwebtoken";
 //login function
 export const login = tryCatch(async (req, res) => {
   const { email, password } = req.body;
@@ -19,14 +20,27 @@ export const login = tryCatch(async (req, res) => {
   if (user.rows.length === 0) {
     throw new AppError("User does not exist", 401);
   }
+  
+ 
   // Check if password is correct
   bcrypt.compare(password, user.rows[0].password, (err, result) => {
     if (!result) {
       throw new AppError("Invalid password", 401);
     }
+    console.log(process.env.JWT_SECRET);
+    
+    // Create token
+    const token = jwt.sign({ id: user.rows[0].id }, process.env.JWT_SECRET, {
+      expiresIn: process.env.JWT_EXPIRES_IN,
+    });
+    // Send token in cookie
+    res.cookie('token', token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production',
+    });
     res.status(200).json({
       status: "success",
-      message: "Logged in successfully",
+      message: "Logged in successfully"
     });
   });
 });

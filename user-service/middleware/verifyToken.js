1b061aa18d696f494b0f93a00dc7f0fdebdbc20f
azdine <azdineserhani1@gmail.com>
2025-01-14 15:50:38 +0100

    add admin verification middleware to enhance authorization checks

--- user-service/middleware/verifyToken.js
+++ user-service/middleware/verifyToken.js
@@ -1,5 +1,7 @@
 import jwt from "jsonwebtoken";
 import AppError from "../../auth-service/utils/AppError.js";
+import { tryCatch } from "../utils/tryCatch.js";
+import db from "../config/db.js";
 
 export const verifyToken = (req, res, next) => {
   try {
@@ -15,8 +17,6 @@ export const verifyToken = (req, res, next) => {
         throw new AppError("Invalid token", 401);
       }
 
-   
-
       req.user = user;
     });
     next();
@@ -26,13 +26,25 @@ export const verifyToken = (req, res, next) => {
 };
 export const verifyAuth = (req, res, next) => {
   verifyToken(req, res, () => {
-    
-    
     if (req.user.id != req.params.id) {
-      
       throw new AppError("You are not authorized", 401);
     }
     next();
-
   });
 };
+
+export const verifyAdmin = tryCatch((req, res, next) => {
+  verifyToken(req, res, async () => {
+    const userId = req.user.id;
+    const userRole = await db.query(
+      "SELECT * FROM user_roles WHERE id = $1",
+      [userId]
+    );
+   
+
+    if (userRole.rows[0].role !== "admin") {
+      throw new AppError("You are not authorized", 401);
+    }
+    next();
+  });
+});

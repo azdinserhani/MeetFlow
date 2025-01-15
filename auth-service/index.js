6ae9ac70194086f8d3b8aa27bd51f011daab878a
azdine <azdineserhani1@gmail.com>
2025-01-15 11:30:17 +0100

    refactor routing to use root path for auth and add express-rate-limit dependency

--- auth-service/index.js
+++ auth-service/index.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 
-app.use("/api/v1/auth", authRoute);
+app.use("/", authRoute);
 app.use(errorHandler);
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);

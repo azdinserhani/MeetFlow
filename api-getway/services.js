3a8d715e4d6456060698646a443d841e87b00293
azdine <azdineserhani1@gmail.com>
2025-01-17 11:38:46 +0100

    add team route to services for task management

--- api-getway/services.js
+++ api-getway/services.js
@@ -7,4 +7,8 @@ export const services = [
     route: "/api/v1/user",
     target: process.env.USER_SERVICE_URL,
   },
+  {
+    route: "/api/v1/team",
+    target: process.env.TASK_MANAGEMENT_URL,
+  },
 ];
